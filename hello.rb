7e8326a52ba1b7ec2ed2d39ae7ea48ae553284f8
ZeenatLawal <zeenatlawal82@gmail.com>
2021-04-19 14:54:53 +0100

    use array methods

--- hello.rb
+++ hello.rb
@@ -34,3 +34,15 @@ elsif name == "zeenat"
 else
   puts "correct"
 end
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+if arr.include?(number)
+  puts "#{number} is in the array"
+else
+  puts "#{number} is not in the array"
+end
+
+arr.each_with_index do |val, idx| 
+  puts "#{idx}- #{val}"
+end
