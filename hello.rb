7b66e6127c57b857f4205e48955dcd0260dc2b64
ZeenatLawal <zeenatlawal82@gmail.com>
2021-04-19 13:24:22 +0100

    use if elsif else statement

--- hello.rb
+++ hello.rb
@@ -25,3 +25,12 @@ end
 if 1 < 2
   puts "Hot diggity, 1 is less than 2!"
 end
+
+name = "zee"
+if name == "ze" 
+  puts "try again"
+elsif name == "zeenat"
+  puts "once more"
+else
+  puts "correct"
+end
